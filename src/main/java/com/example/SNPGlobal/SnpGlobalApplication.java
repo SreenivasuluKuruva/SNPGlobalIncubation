871861e70dfc7cd385826f45cbcf5b99995ffae6
Sreenivasulu Kuruva <sreenivasulu_kuruva@epam.com>
2023-04-25 16:15:15 +0530

    First commit

---
+++ src/main/java/com/example/SNPGlobal/SnpGlobalApplication.java
@@ -0,0 +1,13 @@
+package com.example.SNPGlobal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnpGlobalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnpGlobalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SNPGlobal/SnpGlobalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SNPGlobal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnpGlobalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
